10db6c67ee9ed42f619a3756f85119332d5c7845
senghongoo <sehong29@gmail.com>
2025-05-30 16:01:05 +0900

    2025-05-30

--- js/sub.js
+++ js/sub.js
@@ -1,4 +1,14 @@
 $(function () {
+    $('.turn_book').css({
+        'transform': 'rotateY(40deg)',
+        'transition': 'transform 0.6s ease'
+    });
+
+    setTimeout(function () {
+        $('.turn_book').css('transform', 'rotateY(0deg)');
+    }, 1500);
+
+
     const $input = $('.searchBar > input');
     const originalPlaceholder = $input.attr('placeholder');
 
@@ -31,4 +41,9 @@ $(function () {
         bnCurrentIndex = (bnCurrentIndex + 1) % $bnImgs.length;
         updateBnBanner();
     });
+
+    $('.turn_book').click(function(){
+        $(this).toggleClass('rotate');                                
+    });
+    
 });
