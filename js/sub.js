6a977449933138529db5e8b25dcbe4cdb915aa23
senghongoo <sehong29@gmail.com>
2025-06-04 17:45:30 +0900

    2025-06-04

--- js/sub.js
+++ js/sub.js
@@ -1,14 +1,4 @@
 $(function () {
-    $('.turn_book').css({
-        'transform': 'rotateY(40deg)',
-        'transition': 'transform 0.6s ease'
-    });
-
-    setTimeout(function () {
-        $('.turn_book').css('transform', 'rotateY(0deg)');
-    }, 1500);
-
-
     const $input = $('.searchBar > input');
     const originalPlaceholder = $input.attr('placeholder');
 
@@ -42,8 +32,70 @@ $(function () {
         updateBnBanner();
     });
 
-    $('.turn_book').click(function(){
-        $(this).toggleClass('rotate');                                
+    $('.turn_book').css({
+        'transform': 'rotateY(40deg)',
+        'transition': 'transform 0.6s ease'
     });
-    
+
+    setTimeout(function () {
+        $('.turn_book').css('transform', 'rotateY(0deg)');
+    }, 1500);
+
+    $('.turn_book').click(function () {
+        $(this).toggleClass('rotate');
+    });
+
+    $('.yDetailBnWrap li').hover(function () {
+        $('.yDetailBnWrap li').removeClass("hovered");
+        $(this).addClass("hovered");
+
+        let n = $(this).index();
+        $(".yDetailBnWrap > div> img").hide().eq(n).show();
+    });
+
+    async function fetchBooks(query) {
+        const params = new URLSearchParams({
+            target: "title",
+            query,
+            size: 50
+        });
+
+        const url = `https://dapi.kakao.com/v3/search/book?${params}`;
+
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                Authorization: "KakaoAK 0184f745f7653961f58f01aaed0ba835"
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP 오류! 상태 코드: ${response.status}`);
+        }
+
+        return response.json();
+    };
+
+    async function bookData() {
+        try {
+            const nomi = ['단 한 번의 삶', '빛과 실', '우리의 낙원에서 만나자', '뭐 어때', '이처럼 사소한 것들', '맡겨진 소녀'];
+
+            nomi.forEach(async (query, i) => {
+                const data = await fetchBooks(query);
+                const divs = $('.nomi_list li');
+
+                divs.eq(i).append(`
+                    <img src=${data.documents[0].thumbnail}/>
+                    <h4>${data.documents[0].title}</h4>
+                    <h5>${data.documents[0].authors} 저 | ${data.documents[0].publisher}</h5>
+                    <h4>${data.documents[0].price*0.9}원 (10% 할인)</h4>
+                    `);
+            })
+
+        } catch (error) {
+            console.log('에러발생', error);
+        }
+    };
+
+    bookData();
 });
